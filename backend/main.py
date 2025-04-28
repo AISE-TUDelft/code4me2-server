bd1c52474aee4320d3dc03cec9bb606c23f2a9ba
Parham Bateni <m.bateni@student.tudelft.nl>
2025-04-28 11:32:24 +0200

    [CI/CD] reformatted previously added python files

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
-from .routers import (
-    auth_router
-)
+from .routers import auth_router
 
 app = FastAPI(
     title="Code4Me V2 API",
     description="The complte API for Code4Me V2",
-    version="1.0.0"
+    version="1.0.0",
 )
 
-app.include_router(
-    auth_router
-)
+app.include_router(auth_router)
+
 
 @app.get("/")
 def read_root():
