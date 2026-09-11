28840d599b951d11ec750e3f0bee968d9cf5da1b
myildirim19 <myildirim19@ku.edu.tr>
2026-09-11 14:38:07 +0300

    chore:temp version file

---
+++ src/code4me2_agent/_build_version.py
@@ -0,0 +1,7 @@
+"""Runtime version used for source checkouts.
+
+Release and local ZIP builds temporarily replace this value with their immutable
+artifact version via ``packaging/stamp_runtime_version.py``.
+"""
+
+RUNTIME_VERSION = "0.0.0-dev"
